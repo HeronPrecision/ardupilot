c887fd75b383c7b73eed876b7cea82c942318b3e
Anonymous <user@example.com>
2025-12-16 19:27:30 -0500

    better build.sh file

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf --board=HRON-Chickadee copter --debug"
+#!/usr/bin/env bash
+if [ "$1" == "clean" ]; then
+    docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf clean"
+elif [ "$1" == "bootloader" ]; then
+    docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf bootloader"
+else
+    docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf --board=HRON-Chickadee copter --debug"
+fi
