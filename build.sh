ff33574c2d69d618dac737fd4c667df7cb86896a
Anonymous <user@example.com>
2026-03-20 12:47:59 -0400

    slogging along

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 if [ "$1" == "clean" ]; then
-    docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf clean"
+    rm -rf build/HRON-Chickadee
+    #docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf clean"
 elif [ "$1" == "bootloader" ]; then
     docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf bootloader"
+elif [ "$1" == "configure" ]; then
+    docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf configure --board=HRON-Chickadee"
 else
     docker run --rm -v $(pwd):/ardupilot ardupilot-build /bin/bash -c "cd /ardupilot && ./waf --board=HRON-Chickadee copter --debug"
 fi
